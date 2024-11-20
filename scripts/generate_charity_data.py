da66cb558e92862d445f99cf57b79681ba60190b
mohamedmeqlad9 <mohamedmeqlad9@gmail.com>
2024-11-20 02:44:10 +0200

    update files and create the donors generaet script

--- scripts/generate_charity_data.py
+++ scripts/generate_charity_data.py
@@ -4,9 +4,28 @@ import faker
 import os
 from datetime import timedelta , datetime
 
-faker = faker()
+fake = faker()
 
 def random_date():
       start_date = datetime.now() - timedelta(days=5*365)
       random_days = random.randint(0, 5*365)
       return (start_date + timedelta(days=random_days)).strftime("%Y-%m-%d")
+output_dir = "/home/meqlad/pr/charity_data_engineering_project/data"
+os.makedirs(output_dir, exist_ok=True)
+
+num_donors = 500
+num_donations = 1000
+num_campaigns = 100
+num_beneficiaries = 200
+
+donors_file_path = os.path.join(output_dir, "donors.csv")
+with open(donors_file_path, mode="w", newline="") as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(["donor_id", "donor_name", "email", "phone", "registration_date"])
+    for _ in range(num_donors):
+        donor_id = fake.uuid4()
+        donor_name = fake.name()
+        email = fake.email()
+        phone = fake.phone_number()
+        registration_date = random_date()
+        writer.writerow([donor_id, donor_name, email, phone, registration_date])
