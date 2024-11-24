f3bee8faeeb4f88f08fa98e684d2c4bd244c4ac8
mohamedmeqlad9 <mohamedmeqlad9@gmail.com>
2024-11-24 04:00:21 +0200

    update process_blob_data.py

--- scripts/process_blob_data.py
+++ scripts/process_blob_data.py
@@ -2,3 +2,25 @@ import os
 from azure.storage.blob import BlobServiceClient
 import pandas as pd
 from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+
+
+blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+
+raw_container_client = blob_service_client.get_container_client("raw")
+curated_container_client = blob_service_client.get_container_client("curated")
+
+
+blob_list = raw_container_client.list_blobs()
+
+
+for blob in blob_list:
+
+    raw_blob_client = raw_container_client.get_blob_client(blob.name)
+    download_stream = raw_blob_client.download_blob()
