70c2905da46e58350b4e9cd2174489aebbaf68c2
mohamedmeqlad9 <mohamedmeqlad9@gmail.com>
2024-11-24 04:02:37 +0200

    update process_blob_data.py

--- scripts/process_blob_data.py
+++ scripts/process_blob_data.py
@@ -37,3 +37,25 @@ for blob in blob_list:
 
     df_cleaned.fillna({'column_name': 'Unknown'}, inplace=True)  
     df_cleaned.dropna(inplace=True)  
+
+
+    if 'date' in df_cleaned.columns:
+        df_cleaned['year'] = df_cleaned['date'].dt.year
+        df_cleaned['month'] = df_cleaned['date'].dt.month
+
+ 
+    df_cleaned.rename(columns={'old_column_name': 'new_column_name'}, inplace=True)
+
+
+    processed_filename = f"processed_{blob.name}"
+    df_cleaned.to_csv(processed_filename, index=False)
+
+
+    curated_blob_client = curated_container_client.get_blob_client(processed_filename)
+    with open(processed_filename, "rb") as data:
+        curated_blob_client.upload_blob(data, overwrite=True)
+
+
+    os.remove(processed_filename)
+
+    print(f"Processed and uploaded: {processed_filename}")
